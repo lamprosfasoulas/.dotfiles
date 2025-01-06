31a765e112c15d134d476e4e5e2773866978dc23
Lampros Fasoulas <lfasoula@auth.gr>
2025-01-06 21:21:32 +0200

    minor changes

--- nvim/.config/nvim/lua/lampros/remap.lua
+++ nvim/.config/nvim/lua/lampros/remap.lua
@@ -11,8 +11,8 @@ vim.keymap.set({"n", "v"}, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "zz", "ZZ")
-vim.keymap.set("n", "zq", "ZQ")
+-- vim.keymap.set("n", "zz", "ZZ")
+-- vim.keymap.set("n", "zq", "ZQ")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set({"n","v"}, "<leader>d",[["_d]])
